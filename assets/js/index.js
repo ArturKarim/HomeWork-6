b34ac5e29012d7ecfa0910ebc4d8744cb1bbe6cf
ArturKarim <starhunter73@ymail.com>
2024-12-12 00:33:23 +0400

    add js

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+let button = document.getElementById('button');
+let overlay = document.getElementById('overlay');
+function closePopup(){
+overlay.style.display ='none'
+}
+button.addEventListener('click',closePopup);
